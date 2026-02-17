dc620ace6d60edf6b366f907212d233884678c2e
Maxwell Adomako <gibboel5@gmail.com>
2026-02-17 13:34:53 +0000

    Add Terraform backend configuration for S3 storage

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+
+  backend "s3" {
+    bucket       = "account-vending-terraform-state"
+    key          = "data-bi/terraform.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
+  }
+}
